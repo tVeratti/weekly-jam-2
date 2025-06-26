48c7f9b137f525460b722f842fb2632b539f4795
Tatiana Veratti <TatianaVeratti@gmail.com>
2025-06-26 15:46:11 -0500

    rework line_segment into line_points

---
+++ src/weekly-jam-2/map/line_point.gd
@@ -0,0 +1,6 @@
+class_name LinePoint
+extends Node2D
+
+
+func _ready():
+	hide()
--- src/weekly-jam-2/map/map.gd
+++ src/weekly-jam-2/map/map.gd
@@ -1,23 +1,30 @@
 extends Node2D
 
 
-const LOCAL_POINT_DISTANCE:float = 800.0
+const MAX_HANDLE_X:float = 50.0
+const LOCAL_POINT_DISTANCE:float = 960.0
+const CURVE_ANIMATION_SPEED:float = 4.0
 
 
+@onready var input_component:InputComponent = %InputComponent
+
+# Floor / Line Body
+@onready var static_body_2d = $StaticBody2D
 @onready var collision_polygon:CollisionPolygon2D = %CollisionPolygon2D
 @onready var line_2d:Line2D = %Line2D
-@onready var line_segment:Path2D = %LineSegment
+
+# Animated Line Points
 @onready var curve_animation:AnimationPlayer = %CurveAnimation
-@onready var input_component:InputComponent = %InputComponent
-@onready var static_body_2d = $StaticBody2D
+@onready var line_points: = get_tree().get_nodes_in_group("line_point")
 
 
-var curve_speed:float = 10.0
+var curve_speed:float = CURVE_ANIMATION_SPEED
 var baked_points:PackedVector2Array = []
 
 var ball:RigidBody2D
 var ball_position:Vector2
-var line_position:Vector2
+var line_positions:Array = []
+
 var thread: = Thread.new()
 
 
@@ -26,49 +33,55 @@ func _ready() -> void:
 	
 	curve_animation.current_animation = "curve"
 	curve_animation.pause()
-	
-	var baked_polygon: = LineSegment.get_baked_polygon(baked_points)
-	collision_polygon.polygon = baked_polygon
 
 
 func _process(delta):
+	line_positions = _get_line_positions()
+	
 	if not thread.is_alive():
-		thread.start(_calculate_localized_points)
+		thread.start(_calculate_localized_points_async)
 
 
 func _physics_process(delta):
 	ball_position = ball.global_position
-	line_position = line_segment.global_position
 	
 	var input_direction:Vector2 = input_component.direction
-	var current_animation_position:float = curve_animation.current_animation_position
-	var next_animation_position:float = current_animation_position + (-input_direction.y * delta * 3.0)
-	next_animation_position = clamp(next_animation_position, 0.0, 1.0)
-	curve_animation.seek(next_animation_position, true)
+	_seek_animation(curve_animation, -input_direction.y, delta)
 	
-	static_body_2d.global_position.x += input_direction.x * delta * 100
 	line_2d.points = baked_points
 	
 	var baked_polygon: = LineSegment.get_baked_polygon(baked_points)
 	collision_polygon.polygon = baked_polygon
 
 
-func _calculate_localized_points() -> void:
-	var localized_curve: = Curve2D.new()
-	localized_curve.bake_interval = 30.0
+## Update line_points node positions by seeking AnimationPlayer animation position.
+func _seek_animation(player:AnimationPlayer, input_amount:float, delta:float) -> void:
+	var current_animation_position:float = player.current_animation_position
+	var next_animation_position:float = current_animation_position + (input_amount * delta * curve_speed)
 	
-	for i in line_segment.curve.point_count:
-		var point:Vector2 = line_segment.curve.get_point_position(i) - line_position
-		
+	next_animation_position = clamp(next_animation_position, 0.0, 1.0)
+	player.seek(next_animation_position, true)
+
+
+## Create a curve using `line_positions`, set `baked_points` to be used in `_physics_process`
+func _calculate_localized_points_async() -> void:
+	var localized_curve: = Curve2D.new()
+	localized_curve.bake_interval = 50.0
+	
+	for point in line_positions:
 		if ball_position.distance_to(point) <= LOCAL_POINT_DISTANCE:
-			var point_in = line_segment.curve.get_point_in(i)
-			var point_out = line_segment.curve.get_point_out(i)
+			var point_in: = Vector2(-MAX_HANDLE_X, 0)
+			var point_out: = Vector2(MAX_HANDLE_X, 0)
 			localized_curve.add_point(point, point_in, point_out)
 	
-	
 	baked_points = localized_curve.get_baked_points()
+	
 	call_deferred("_localized_points_ready")
 
 
 func _localized_points_ready() -> void:
 	thread.wait_to_finish()
+
+
+func _get_line_positions() -> Array:
+	return line_points.map(func(node): return node.global_position)
